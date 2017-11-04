7e40d0469467abff57b848716a82af94fdfdb11c
Aksu, Tacettin (CT DD DS EU TR DA) <tacettin.aksu@siemens.com>
2017-11-04 17:05:40 +0300

    enrich example with array and map data types

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	var myInt int
 	myInt = 42
@@ -17,4 +19,38 @@ func main() {
 	println(real(myComplex))
 	println(imag(myComplex))
 
+	myArray := [...]int{28, 53, 61, 52, 29}
+
+	fmt.Println("")
+	fmt.Println("------------ ARRAYS -----------------------------")
+
+	myArray[2] = 19
+	//myArray[5] = 23     //-> ERROR on compile time
+
+	fmt.Println("myArray ", myArray)
+
+	mySlice := myArray[:]
+
+	fmt.Println("mySlice full      : ", mySlice)
+
+	mySlice = myArray[2:4]
+
+	fmt.Println("mySlice 2:4       : ", mySlice)
+
+	mySlice = append(mySlice, 100)
+
+	fmt.Println("mySlice appended  : ", mySlice)
+
+	fmt.Println("")
+	fmt.Println("---------------   MAPS   ------------------------------------")
+
+	myMap := make(map[int]string)
+
+	fmt.Println("myMap empty  : ", myMap)
+
+	myMap[42] = "Foo"
+	myMap[12] = "Bar"
+
+	fmt.Println("myMap        : ", myMap)
+	fmt.Println("myMap key 12 : ", myMap[12])
 }

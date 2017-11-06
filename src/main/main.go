bf57e0d65b3044f42dda78a830e31649f026183c
Aksu, Tacettin (CT DD DS EU TR DA) <tacettin.aksu@siemens.com>
2017-11-06 20:43:13 +0300

    first go routine example

--- src/main/main.go
+++ src/main/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() { 
-	fmt.Println("")
-	fmt.Println("=====================================================")
+func main() {
+	//go routine
+	go func() {
+		for i := 0; i < 150; i++ {
+			fmt.Println("Go Johnny Go")
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 150; i++ {
+			fmt.Println("Back to future!")
+		}
+	}()
+
+	dur, _ := time.ParseDuration("1s")
+	time.Sleep(dur)
 }
